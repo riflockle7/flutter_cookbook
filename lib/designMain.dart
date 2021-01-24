14029a6fefbb1105b6bda24ed7ec8e6cd63766c3
riflockle7 <ridickle7@gmail.com>
2021-01-25 04:38:29 +0900

    #2 Use themes to share colors and font styles
    https://flutter.dev/docs/cookbook/design/themes

--- lib/designMain.dart
+++ lib/designMain.dart
@@ -8,7 +8,10 @@ void main() {
   // runApp(SnackBarDemo());
 
   // Update the UI based on orientation
-  runApp(OrientationApp());
+  // runApp(OrientationApp());
+
+  // Use themes to share colors and font styles
+  runApp(ThemeColorFontApp());
 }
 
 // Add a Drawer to a screen
@@ -149,6 +152,72 @@ class OrientationList extends StatelessWidget {
             }),
           );
         },
+      ),
+    );
+  }
+}
+
+// Use themes to share colors and font styles
+class ThemeColorFontApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appName = 'Custom Themes';
+
+    return MaterialApp(
+      title: appName,
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+
+        // Define the default font family.
+        fontFamily: 'Georgia',
+
+        // Define the default TextTheme. Use this to specify the default
+        // text styling for headlines, titles, bodies of text, and more.
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      home: ThemeColorFontPage(
+        title: appName,
+      ),
+    );
+  }
+}
+
+class ThemeColorFontPage extends StatelessWidget {
+  final String title;
+
+  ThemeColorFontPage({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
+          color: Theme.of(context).accentColor,
+          child: Text(
+            'Text with a background color',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ),
+      floatingActionButton: Theme(
+        data: Theme.of(context).copyWith(
+          colorScheme:
+          Theme.of(context).colorScheme.copyWith(secondary: Colors.yellow),
+        ),
+        child: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
