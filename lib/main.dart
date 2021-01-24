6376d852ebfc79904f114ee58fa7e2219f658091
riflockle7 <ridickle7@gmail.com>
2021-01-25 02:52:45 +0900

    #1 Animate a page route transition
    https://flutter.dev/docs/cookbook/animation/page-route-animation

--- lib/main.dart
+++ lib/main.dart
@@ -25,8 +25,19 @@ class Page1 extends StatelessWidget {
 Route _createRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => Page2(),
+    // animation 은 0과 1 사이의 Double 값을 가지는 Animation<Double> 타입형 변수이다.
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
+      var begin = Offset(0, 1);
+      var end = Offset.zero;
+      var curve = Curves.ease;
+
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+      // pageBuilder 에서 반환한 위젯
+      return SlideTransition(
+        position: animation.drive(tween),
+        child: child,
+      );
     },
   );
 }
