95d62beb6040a3cf63c30c89dda68c84d1b1534a
riflockle7 <ridickle7@gmail.com>
2021-01-31 23:48:47 +0900

    #6 Handle taps
    https://flutter.dev/docs/cookbook/gestures/handling-taps

--- lib/gestureMain.dart
+++ lib/gestureMain.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   // Add Material touch ripples
-  runApp(MyApp());
+  // runApp(MyApp());
+
+  // Handle taps
+  runApp(GestureDetectorApp());
 }
 
 // Add Material touch ripples
@@ -50,5 +54,93 @@ class RippleButton extends StatelessWidget {
         child: Text('Flat Button'),
       ),
     );
+  }
+}
+
+// Handle taps
+class GestureDetectorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Gesture Demo';
+
+    return MaterialApp(
+      title: title,
+      home: GestureDetectorPage(title: title),
+    );
+  }
+}
+
+class GestureDetectorPage extends StatelessWidget {
+  final String title;
+
+  GestureDetectorPage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(child: GestureDetectorButton()),
+    );
+  }
+}
+
+class GestureDetectorButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // GestureDetector 가 버튼을 감쌈 (child)
+    return GestureDetector(
+        // 탭 할 떄 스낵바 올라옴
+        onTap: () {
+          final snackBar = SnackBar(content: Text("Tap"));
+
+          Scaffold.of(context).showSnackBar(snackBar);
+        },
+        // 커스텀 버튼
+        child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, index) => generateRows(context, index)));
+  }
+
+  Widget generateRows(BuildContext context, int type) {
+    switch (type) {
+      case 0:
+        return RaisedButton(
+          onPressed: () {
+            final snackBar = SnackBar(content: Text("RaisedButton Clicked"));
+
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+          child: Text("Raised Button"),
+        );
+      case 1:
+        return ElevatedButton(
+          onPressed: () {
+            final snackBar = SnackBar(content: Text("ElevatedButton Clicked"));
+
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+          child: Text('Elevated Button'),
+        );
+      case 2:
+        return CupertinoButton(
+          onPressed: () {
+            final snackBar = SnackBar(content: Text("CupertinoButton Clicked"));
+
+            Scaffold.of(context).showSnackBar(snackBar);
+          },
+          child: Text("Cupertino Button"),
+        );
+      default:
+        return Container(
+          padding: EdgeInsets.all(12.0),
+          decoration: BoxDecoration(
+            color: Theme.of(context).buttonColor,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: Text('My Button'),
+        );
+    }
   }
 }
