fafeacb10d7cd9575fd7b5208c6ffe4a08629db9
riflockle7 <ridickle7@gmail.com>
2021-02-01 01:16:07 +0900

    #7 Fade in images with a placeholder
    https://flutter.dev/docs/cookbook/images/fading-in-images

--- lib/imageMain.dart
+++ lib/imageMain.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 void main() {
   // Display images from the internet
-  runApp(InternetImageApp());
+  // runApp(InternetImageApp());
+
+  // Fade in images with a placeholder
+  runApp(PlaceHolderFadeApp());
 }
 
 // Display images from the internet
@@ -19,6 +23,33 @@ class InternetImageApp extends StatelessWidget {
           title: Text(title),
         ),
         body: Image.network('https://picsum.photos/250?image=9'),
+      ),
+    );
+  }
+}
+
+// Fade in images with a placeholder
+class PlaceHolderFadeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Fade in images';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image: 'https://picsum.photos/250?image=9',
+          ),
+          // child: FadeInImage.assetNetwork(
+          //   placeholder: 'assets/loading.gif',
+          //   image: 'https://picsum.photos/250?image=9',
+          // ),
+        ),
       ),
     );
   }
