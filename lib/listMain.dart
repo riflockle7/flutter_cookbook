7393d36e9d10356c225e8fa4f362b74861469e52
riflockle7 <ridickle7@gmail.com>
2021-02-08 17:33:06 +0900

    #10 Create a horizontal list
    https://flutter.dev/docs/cookbook/lists/horizontal-list

--- lib/listMain.dart
+++ lib/listMain.dart
@@ -3,8 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   // Create a grid list
   runApp(GridListApp());
+
+  // Create a horizontal list
+  runApp(HorizontalApp());
 }
 
+// Create a grid list
 class GridListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,6 +32,53 @@ class GridListApp extends StatelessWidget {
               ),
             );
           }),
+        ),
+      ),
+    );
+  }
+}
+
+// Create a horizontal list
+class HorizontalApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = 'Horizontal List';
+
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: ListView(
+            // 이걸로 인해 수평 정렬이 가능해짐
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.orange,
+              ),
+            ],
+          ),
         ),
       ),
     );
